c1b580579c07ad335b8f81bfc6b92afee51d2315
Emmanuel Donate <e.o.donate@gmail.com>
2022-10-13 19:16:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Hello...MotherBoard!
         </a>
       </header>
     </div>
